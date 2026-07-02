41bae85a52d9d6e8c183f663df1aa203a398f752
minitoontvinfo-oss <minitoontv.info@gmail.com>
2026-07-02 09:40:28 +0700

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,16 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// Khởi tạo Firebase trong background
 firebase.initializeApp({
   apiKey: "AIzaSyDoBoeAyxWeoYiAQkudVi8ZS3uQJVGvjXc",
-  authDomain: "app-learning-english-60a96.firebaseapp.com",
   projectId: "app-learning-english-60a96",
-  storageBucket: "app-learning-english-60a96.firebasestorage.app",
-  messagingSenderId: "248531393253",
-  appId: "1:248531393253:web:d3afdcbedf576445dd920c"
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abc"
 });
 
 const messaging = firebase.messaging();
-
-// Xử lý khi nhận thông báo ngầm
-messaging.onBackgroundMessage(function(payload) {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: 'https://via.placeholder.com/150' // Thay bằng Logo Ostock
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  });
 });
